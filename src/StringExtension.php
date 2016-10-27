c6279ae064a187694fb62bfb0602c152b48745ca
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-10-27 20:01:19 +0300

    Новый StringExtension Фильтры cleartags и respace

---
+++ src/StringExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ruvents\TwigExtensions;
+
+class StringExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('cleartags', function ($string) {
+                return preg_replace('/<([a-z][a-z0-9]*)[^>]*?(\/?)>/i', '<$1$2>', $string);
+            }),
+            new \Twig_SimpleFilter('respace', function ($string, $mode = 's') {
+                return preg_replace("/\p{Z$mode}+/ui", ' ', $string);
+            }),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'ruvents_twig_extensions.string';
+    }
+}

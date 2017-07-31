3746057fa6ff34b0a887a8b4f39f14d655f52f75
Valentin <udaltsov.valentin@gmail.com>
2017-07-31 20:11:50 +0300

    SymfonyIntlExtension / intl_(currency|currency_symbol|language|locale|script) filters + tests

--- src/SymfonyIntlExtension.php
+++ src/SymfonyIntlExtension.php
@@ -21,14 +21,59 @@ class SymfonyIntlExtension extends AbstractExtension
     public function getFilters()
     {
         return [
-            new TwigFilter('intl_country', function ($name, $locale = null) {
-                if (is_string($name)) {
-                    return Intl::getRegionBundle()->getCountryName($name, $locale);
+            new TwigFilter('intl_country', function ($country, string $locale = null) {
+                if (is_string($country)) {
+                    return Intl::getRegionBundle()->getCountryName($country, $locale);
                 }
 
-                return array_map(function ($name) use ($locale) {
-                    return Intl::getRegionBundle()->getCountryName($name, $locale);
-                }, Helper::iterableToArray($name));
+                return array_map(function ($country) use ($locale) {
+                    return Intl::getRegionBundle()->getCountryName($country, $locale);
+                }, Helper::iterableToArray($country));
+            }),
+            new TwigFilter('intl_currency', function ($currency, string $locale = null) {
+                if (is_string($currency)) {
+                    return Intl::getCurrencyBundle()->getCurrencyName($currency, $locale);
+                }
+
+                return array_map(function ($currency) use ($locale) {
+                    return Intl::getCurrencyBundle()->getCurrencyName($currency, $locale);
+                }, Helper::iterableToArray($currency));
+            }),
+            new TwigFilter('intl_currency_symbol', function ($currency, string $locale = null) {
+                if (is_string($currency)) {
+                    return Intl::getCurrencyBundle()->getCurrencySymbol($currency, $locale);
+                }
+
+                return array_map(function ($currency) use ($locale) {
+                    return Intl::getCurrencyBundle()->getCurrencySymbol($currency, $locale);
+                }, Helper::iterableToArray($currency));
+            }),
+            new TwigFilter('intl_language', function ($language, string $region = null, string $locale = null) {
+                if (is_string($language)) {
+                    return Intl::getLanguageBundle()->getLanguageName($language, $region, $locale);
+                }
+
+                return array_map(function ($language) use ($region, $locale) {
+                    return Intl::getLanguageBundle()->getLanguageName($language, $region, $locale);
+                }, Helper::iterableToArray($language));
+            }),
+            new TwigFilter('intl_locale', function ($locale, string $displayLocale = null) {
+                if (is_string($locale)) {
+                    return Intl::getLocaleBundle()->getLocaleName($locale, $displayLocale);
+                }
+
+                return array_map(function ($locale) use ($displayLocale) {
+                    return Intl::getLocaleBundle()->getLocaleName($locale, $displayLocale);
+                }, Helper::iterableToArray($locale));
+            }),
+            new TwigFilter('intl_script', function ($script, string $language = null, string $locale = null) {
+                if (is_string($script)) {
+                    return Intl::getLanguageBundle()->getScriptName($script, $language, $locale);
+                }
+
+                return array_map(function ($script) use ($language, $locale) {
+                    return Intl::getLanguageBundle()->getScriptName($script, $language, $locale);
+                }, Helper::iterableToArray($script));
             }),
         ];
     }

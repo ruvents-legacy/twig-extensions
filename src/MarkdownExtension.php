44661dfc97406274892797ae9efe7553ed07c5e4
Valentin <udaltsov.valentin@gmail.com>
2017-07-31 18:22:43 +0300

    MarkdownExtension / Allow to override default Parsedown config

--- src/MarkdownExtension.php
+++ src/MarkdownExtension.php
@@ -30,12 +30,37 @@ class MarkdownExtension extends AbstractExtension
     public function getFilters()
     {
         return [
-            new TwigFilter('md_line', function ($string) {
-                return $this->parsedown->line($string);
-            }, ['is_safe' => ['html']]),
-            new TwigFilter('md_text', function ($string) {
-                return $this->parsedown->text($string);
-            }, ['is_safe' => ['html']]),
+            new TwigFilter('md_line',
+                function ($string, bool $lineBreaks = null, bool $html = null, bool $autoUrls = null) {
+                    return $this->createParsedown($lineBreaks, $html, $autoUrls)->line($string);
+                },
+                ['is_safe' => ['html']]
+            ),
+            new TwigFilter('md_text',
+                function ($string, bool $lineBreaks = null, bool $html = null, bool $autoUrls = null) {
+                    return $this->createParsedown($lineBreaks, $html, $autoUrls)->text($string);
+                },
+                ['is_safe' => ['html']]
+            ),
         ];
+    }
+
+    private function createParsedown(bool $lineBreaks = null, bool $html = null, bool $autoUrls = null): \Parsedown
+    {
+        $parsedown = clone $this->parsedown;
+
+        if (null !== $lineBreaks) {
+            $parsedown->setBreaksEnabled($lineBreaks);
+        }
+
+        if (null !== $html) {
+            $parsedown->setMarkupEscaped(!$html);
+        }
+
+        if (null !== $autoUrls) {
+            $parsedown->setUrlsLinked($autoUrls);
+        }
+
+        return $parsedown;
     }
 }
